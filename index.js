00101f2419b1bf5e1d7af4a738741601c540b2fa
Tony Lam <Tonylam750@outlook.com>
2025-06-25 22:37:41 +0200

    refactored renderMovies func for better api fetch performance

--- index.js
+++ index.js
@@ -8,10 +8,13 @@ async function renderMovies() {
     const data = await response.json()
     let searchHtml = ""
 
-    for (let movie of data.Search) {
-        const res = await fetch(`https://www.omdbapi.com/?apikey=ab7d24d4&t=${movie.Title}`)
-        const movieDetail = await res.json()
+    const detailPromises = data.Search.map(movie =>
+        fetch(`https://www.omdbapi.com/?apikey=ab7d24d4&t=${movie.Title}`).then(r => r.json())
+    );
+    const details = await Promise.all(detailPromises);
 
+    data.Search.forEach((movie, i) => {
+        const movieDetail = details[i]
         let html = `
             <div class="movie-content">
                 <img src="${movie.Poster}">
@@ -27,7 +30,7 @@ async function renderMovies() {
             </div>`
 
         searchHtml += html
-    };
+    });
 
     document.getElementById("main").innerHTML = searchHtml
     HandleAddToWatchList()
@@ -56,91 +59,5 @@ function HandleAddToWatchList() {
     })
 }
 
-
-
-
-
 searchBtn.addEventListener("click", renderMovies)
 
-// Poster
-// :
-// "https://m.media-amazon.com/images/M/MV5BODIyMDdhNTgtNDlmOC00MjUxLWE2NDItODA5MTdkNzY3ZTdhXkEyXkFqcGc@._V1_SX300.jpg"
-// Title
-// :
-// "Batman Begins"
-// Type
-// :
-// "movie"
-// Year
-// :
-// "2005"
-// imdbID
-// :
-// "tt0372784"
-
-// Actors
-// :
-// "Kevin Conroy, Loren Lester, Efrem Zimbalist Jr."
-// Awards
-// :
-// "Won 1 Primetime Emmy. 5 wins & 19 nominations total"
-// Country
-// :
-// "United States"
-// Director
-// :
-// "N/A"
-// Genre
-// :
-// "Animation, Action, Adventure"
-// Language
-// :
-// "English"
-// Metascore
-// :
-// "N/A"
-// Plot
-// :
-// "Follows the adventures of billionaire playboy Bruce Wayne who is secretly the vigilante known as Batman, whom with help from various side kicks and allies fights a rouges gallery of criminals and super-villains in Gotham City."
-// Poster
-// :
-// "https://m.media-amazon.com/images/M/MV5BYjgwZWUzMzUtYTFkNi00MzM0LWFkMWUtMDViMjMxNGIxNDUxXkEyXkFqcGc@._V1_SX300.jpg"
-// Rated
-// :
-// "TV-PG"
-// Ratings
-// :
-// [{…}]
-// Released
-// :
-// "05 Sep 1992"
-// Response
-// :
-// "True"
-// Runtime
-// :
-// "23 min"
-// Title
-// :
-// "Batman: The Animated Series"
-// Type
-// :
-// "series"
-// Writer
-// :
-// "Bill Finger, Bob Kane, Eric Radomski"
-// Year
-// :
-// "1992–1995"
-// imdbID
-// :
-// "tt0103359"
-// imdbRating
-// :
-// "9.0"
-// imdbVotes
-// :
-// "127,724"
-// totalSeasons
-// :
-// "4"
